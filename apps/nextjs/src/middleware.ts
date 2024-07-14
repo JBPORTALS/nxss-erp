f71c734f327d5d251c16eb1bd2efbfcc6f657de6
x-sss-x <manu48167@gmail.com>
2024-07-14 13:09:35 +0530

    "Remove sign-in page and update middleware to redirect authenticated users to organization dashboard"

--- apps/nextjs/src/middleware.ts
+++ apps/nextjs/src/middleware.ts
@@ -1,11 +1,29 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { notFound } from "next/navigation";
+import { NextRequest, NextResponse } from "next/server";
+import {
+  clerkClient,
+  clerkMiddleware,
+  createRouteMatcher,
+} from "@clerk/nextjs/server";
 
 const isPublicRoutes = createRouteMatcher(["/sign-in"]);
 
 export default clerkMiddleware(
   async (auth, request) => {
+    const userId = auth().userId;
     if (!isPublicRoutes(request)) {
       auth().protect();
+    }
+
+    //if it matches public routes and authenticated. push to current organization
+    if (userId && isPublicRoutes(request)) {
+      const organizations =
+        await clerkClient().users.getOrganizationMembershipList({ userId });
+
+      const orgslug = organizations.data[0]?.organization.slug;
+      return NextResponse.redirect(
+        new URL(`/${orgslug}/dashboard`, request.nextUrl.origin),
+      );
     }
   },
   {
