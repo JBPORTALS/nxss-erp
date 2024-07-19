ba5fc51c2c9f846c16776efd0a1b8815e3105ab5
Akash143l3 <Team3Nala@gmail.com>
2024-07-19 12:54:45 +0530

    "Added new step to MultiStepForm, updated button clicks and disabled states"

--- packages/ui/src/multi-step-formdemo.tsx
+++ packages/ui/src/multi-step-formdemo.tsx
@@ -28,12 +28,24 @@ const StaffForm = () => {
         <Input placeholder="Acme" />
         <Label>Institution Staff Id</Label>
         <Input placeholder="465UA123" />
-        <span>Upload Your Staff ID for verification</span>
-        <ImageUploader />
         <Button onClick={handleSubmitStep1} disabled={currentStep !== 1}>
           Submit
         </Button>
       </Step>
+
+      <Step
+        variant={currentStep === 2 ? "inProcess" : "inProcess"}
+        disabled={currentStep < 2}
+      >
+        <h1 className="text-2xl font-semibold">Upload Staff ID</h1>
+        <span>Upload Your Staff ID for verification</span>
+        <ImageUploader />
+        <Button onClick={handleSubmitStep2} className="w-40" disabled={currentStep !== 2}>
+          Submit
+        </Button>
+      </Step>
+      
+      
     </MultiStepForm>
   );
 };
