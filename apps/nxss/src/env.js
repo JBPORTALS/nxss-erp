e6aca80df167b7ee99786d95c19beea1c3b1cb2a
x-sss-x <manu48167@gmail.com>
2024-07-24 15:17:16 +0530

    "Remove environment variables from env.js, update package.json scripts in db package, and modify staff schema in db/src/schema/staff.ts"

--- apps/nxss/src/env.js
+++ apps/nxss/src/env.js
@@ -17,10 +17,7 @@ export const env = createEnv({
    * Specify your server-side environment variables schema here.
    * This way you can ensure the app isn't built with invalid env vars.
    */
-  server: {
-    DATABASE_URL: z.string(),
-    CLERK_SECRET_KEY: z.string(),
-  },
+  server: {},
   /**
    * Specify your client-side environment variables schema here.
    * For them to be exposed to the client, prefix them with `NEXT_PUBLIC_`.
@@ -35,11 +32,8 @@ export const env = createEnv({
    * Destructure all variables from `process.env` to make sure they aren't tree-shaken away.
    */
   runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
-    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
     PORT: process.env.PORT,
     VERCEL_URL: process.env.VERCEL_URL,
-
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
       process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
     NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
