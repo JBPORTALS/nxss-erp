8acfbee5b8d1fecdcd2ea3491dd03388c337b213
Harshitha509 <rsharshitha509@gmail.com>
2024-08-01 18:48:52 +0530

    fees,curriculars,test schemas

--- packages/db/src/schema/auth.ts
+++ packages/db/src/schema/auth.ts
@@ -1,5 +1,5 @@
 import { relations } from "drizzle-orm";
-import { integer, serial, text ,timestamp} from "drizzle-orm/pg-core";
+import { integer, serial, text ,timestamp,boolean,date} from "drizzle-orm/pg-core";
 import { pgTable } from "./_table";
 import { statusEnum, patternEnum } from "./enum";
 import { datetime, time } from "drizzle-orm/mysql-core";
@@ -15,17 +15,16 @@ export const institutions = pgTable("institutions", {
 });
 
 // User table
-// export const users = pgTable("users", {
-//   id: text("id").primaryKey(),
-//   full_name: text("full_name").notNull(),
-//   date_of_birth: date("date_of_birth"),
-//   year_of_join: integer("year_of_join"),
-//   phone_num: integer("phone_num"),
-//   is_phone_verified: boolean("is_phone_verified").default(false),
-//   user_type: userTypeEnum("user_type").notNull(),
-//   created_at: timestamp("created_at").defaultNow().notNull(),
-//   updated_at: timestamp("updated_at"),
-// });
+ export const users = pgTable("users", {
+   id: text("id").primaryKey(),
+   full_name: text("full_name").notNull(),
+   date_of_birth: date("date_of_birth"),
+   year_of_join: integer("year_of_join"),
+   phone_num: integer("phone_num"),
+   is_phone_verified: boolean("is_phone_verified").default(false),
+   created_at: timestamp("created_at").defaultNow().notNull(),
+   updated_at: timestamp("updated_at"),
+ });
 
 // Academic Year table
 export const academicYears = pgTable("academic_years", {
