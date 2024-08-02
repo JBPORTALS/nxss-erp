89d68f15df00bfab0a17665e071f8466ed43592c
x-sss-x <manu48167@gmail.com>
2024-08-02 23:04:22 +0530

    "Added Zod import and getBranchDetails procedure to branchesRouter"

--- packages/api/src/router/branch.ts
+++ packages/api/src/router/branch.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+
 import { eq, schema } from "@nxss/db";
 
 import { protectedProcedure, router } from "../trpc";
@@ -21,4 +23,11 @@ export const branchesRouter = router({
 
     return mapped_response;
   }),
+  getBranchDetails: protectedProcedure
+    .input(z.object({ id: z.string().min(1, "Branch ID is required!") }))
+    .query(async ({ ctx, input }) => {
+      return ctx.db.query.branches.findMany({
+        where: eq(branches.id, parseInt(input.id)),
+      });
+    }),
 });
