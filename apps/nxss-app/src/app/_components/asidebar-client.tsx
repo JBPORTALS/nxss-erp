8937cefc17d69ff46900f4f48d2cfa73e4ac0382
Akash143l3 <Team3Nala@gmail.com>
2024-08-05 17:48:54 +0530

    conmiting

--- apps/nxss-app/src/app/_components/asidebar-client.tsx
+++ apps/nxss-app/src/app/_components/asidebar-client.tsx
@@ -1,7 +1,22 @@
 import Link from "next/link";
 import { Protect } from "@clerk/nextjs";
-import { HomeIcon, Layers, PlusCircle, Users2Icon } from "lucide-react";
+import {
+  ArrowLeft,
+  BookMarked,
+  Files,
+  HomeIcon,
+  Layers,
+  Layers2,
+  LayoutDashboard,
+  PlusCircle,
+  Settings,
+  Users2Icon,
+  UsersRound,
+} from "lucide-react";
+
 import { Sidebar, SidebarBody, SidebarLabel } from "@nxss/ui/asidebar";
+import { HStack, VStack } from "@nxss/ui/stack";
+
 import { api } from "~/trpc/server";
 import BranchListClient from "./branch-list-client";
 import { ComboboxDemo } from "./combobox";
@@ -29,8 +44,40 @@ export default async function AsideBarClient({
     <Sidebar>
       <SidebarSwitcher type="subject">
         <SidebarLabel>Subject Menu</SidebarLabel>
-        
-        
+        <VStack className="space-y-4 border-b pb-4">
+          <ComboboxDemo />
+
+          <Link
+            href={`/${params.org}/dashboard`}
+            className="flex gap-3 font-semibold"
+          >
+            <ArrowLeft />
+            Back to Dashboard{" "}
+          </Link>
+        </VStack>
+        <SidebarItemClient path={``}>
+          <LayoutDashboard className="size-4" /> Overview
+        </SidebarItemClient>
+        <SidebarItemClient path={`#`}>
+          <UsersRound className="size-4" />
+          Allocations
+        </SidebarItemClient>
+        <SidebarItemClient path={`#`}>
+          <Files className="size-4" />
+          Notes
+        </SidebarItemClient>
+        <SidebarItemClient path={`#`}>
+          <Layers2 className="size-4" />
+          Assignments
+        </SidebarItemClient>
+        <SidebarItemClient path={`#`}>
+          <BookMarked className="size-4" />
+          Marks
+        </SidebarItemClient>
+        <SidebarItemClient path={`#`}>
+          <Settings className="size-4" />
+          Settings
+        </SidebarItemClient>
       </SidebarSwitcher>
       <SidebarSwitcher type="main">
         <SidebarLabel>MAIN MENU</SidebarLabel>
@@ -45,44 +92,47 @@ export default async function AsideBarClient({
             </SidebarItemClient>
           </Protect>
         </SidebarBody>
-        <Protect role="org:admin">
-          <SidebarLabel className="flex items-center justify-between pr-2">
-            BRANCHES
-            <CreateBranchDailog />
-          </SidebarLabel>
-        </Protect>
-        <Protect role="org:staff">
-          <SidebarLabel className="flex items-center justify-between pr-2">
-            SUBJECTS
-          </SidebarLabel>
-        </Protect>
-        <SidebarBody className="flex flex-grow flex-col overflow-hidden">
-          {/*  */}
-          {hasAccordion ? (
-            <Protect role="org:admin">
-            <BranchListClient {...{ branchList, params }} />
-            </Protect>
-          ) : (
-            <main className="pr-2">
-              <div className="space-y-2 rounded-lg border bg-secondary/10 p-5">
-                <Protect role="org:admin">
-                  <span className="text-sm font-semibold">No Branches</span>
-                  <p className="text-xs text-muted-foreground">
-                    Create new branch by clicking on the BRANCHES plus icon.
-                  </p>
-                </Protect>
-                <Protect role="org:staff">
-                  <span className="text-sm font-semibold">
-                    No Subjects Assigned
-                  </span>
-                  <p className="text-xs text-muted-foreground">
-                    Wait for subject allocation by your institution admin.
-                  </p>
-                </Protect>
-              </div>
-            </main>
-          )}
-        </SidebarBody>
+
+        <div className="space-y-1">
+          <Protect role="org:admin">
+            <SidebarLabel className="flex items-center justify-between pr-2">
+              BRANCHES
+              <CreateBranchDailog />
+            </SidebarLabel>
+          </Protect>
+          <Protect role="org:staff">
+            <SidebarLabel className="flex items-center justify-between pr-2">
+              SUBJECTS
+            </SidebarLabel>
+          </Protect>
+          <SidebarBody className="flex flex-grow flex-col overflow-hidden">
+            {/*  */}
+            {hasAccordion ? (
+              <Protect role="org:admin">
+                <BranchListClient {...{ branchList, params }} />
+              </Protect>
+            ) : (
+              <main className="pr-2">
+                <div className="space-y-2 rounded-lg border bg-secondary/10 p-5">
+                  <Protect role="org:admin">
+                    <span className="text-sm font-semibold">No Branches</span>
+                    <p className="text-xs text-muted-foreground">
+                      Create new branch by clicking on the BRANCHES plus icon.
+                    </p>
+                  </Protect>
+                  <Protect role="org:staff">
+                    <span className="text-sm font-semibold">
+                      No Subjects Assigned
+                    </span>
+                    <p className="text-xs text-muted-foreground">
+                      Wait for subject allocation by your institution admin.
+                    </p>
+                  </Protect>
+                </div>
+              </main>
+            )}
+          </SidebarBody>
+        </div>
       </SidebarSwitcher>
     </Sidebar>
   );
