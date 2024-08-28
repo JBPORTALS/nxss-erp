b90294d7bcf9d283630798c57a82a74b172588c0
x-sss-x <manu48167@gmail.com>
2024-08-28 09:20:45 +0530

    "Update onboarding layout, add RocketIcon, modify app layout and middleware to handle onboarding redirects"

--- apps/nxss-app/src/app/(main)/onboarding/layout.tsx
+++ apps/nxss-app/src/app/(main)/onboarding/layout.tsx
@@ -1,5 +1,7 @@
+import Image from "next/image";
 import { redirect } from "next/navigation";
 import { auth } from "@clerk/nextjs/server";
+import { RocketIcon } from "lucide-react";
 
 export default function RootLayout({
   children,
@@ -16,14 +18,16 @@ export default function RootLayout({
         <div className="h-full w-1/2 border-r">
           <img
             src="/Diamond-up-hat.png"
-            className="absolute left-0 top-0 -z-10"
+            className="absolute left-0 top-0 -z-10 dark:opacity-5"
           />
           <div className="flex h-full items-center justify-center">
-            <img src="/rocket.png" className="h-20 w-20" />
+            <div className="flex size-20 border-spacing-60 items-center justify-center rounded-full border-4 bg-primary outline-2 outline-offset-2 outline-primary-foreground">
+              <RocketIcon className="size-10 text-primary-foreground" />
+            </div>
           </div>
           <img
             src="/Diamond-down-hat.png"
-            className="absolute bottom-0 right-1/2 -z-10"
+            className="absolute bottom-0 right-1/2 -z-10 dark:opacity-5"
           />
         </div>
         {children}
