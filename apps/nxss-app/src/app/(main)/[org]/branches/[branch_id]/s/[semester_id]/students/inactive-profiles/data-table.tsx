0b63ac2649383ee1d899663400928539d76d9afa
x-sss-x <manu48167@gmail.com>
2024-09-21 21:57:33 +0530

    "Rearranged import statements in data-table.tsx"

--- apps/nxss-app/src/app/(main)/[org]/branches/[branch_id]/s/[semester_id]/students/inactive-profiles/data-table.tsx
+++ apps/nxss-app/src/app/(main)/[org]/branches/[branch_id]/s/[semester_id]/students/inactive-profiles/data-table.tsx
@@ -2,13 +2,10 @@
 
 import React from "react";
 
-import { DataTable } from "~/app/_components/data-table";
-import { useActions } from "./actions";
-
-import "@tanstack/react-table";
-
 import { RouterOutputs } from "@nxss/api";
 
+import { DataTable } from "~/app/_components/data-table";
+import { useActions } from "./actions";
 import { useColumns } from "./columns";
 
 export function DataTableClient({
