aa0ad9996dd8b17b5b63e6a6c73db1ce5faf9325
x-sss-x <jbportalsblr@gmail.com>
2025-01-17 18:28:29 +0530

    feat: Enhance student page with search functionality and add student button; update input styles for better UI consistency

--- apps/nxss-app/src/app/(main)/[orgId]/branches/[branchId]/[semesterId]/students/page.tsx
+++ apps/nxss-app/src/app/(main)/[orgId]/branches/[branchId]/[semesterId]/students/page.tsx
@@ -1,3 +1,6 @@
+import { PlusIcon, SearchIcon } from "lucide-react";
+
+import { Button } from "@nxss/ui/button";
 import {
   ContentArea,
   ContentAreaContainer,
@@ -5,6 +8,7 @@ import {
   ContentAreaHeader,
   ContentAreaTitle,
 } from "@nxss/ui/content-area";
+import { Input } from "@nxss/ui/input";
 
 export default async function BranchPage({
   params,
@@ -14,13 +18,26 @@ export default async function BranchPage({
   return (
     <div className="flex h-screen">
       <ContentArea>
-        <ContentAreaHeader>
-          <ContentAreaTitle>Students</ContentAreaTitle>
-          <ContentAreaDescription>
-            All student in this branch & semester
-          </ContentAreaDescription>
+        <ContentAreaHeader className="flex-row items-center justify-between">
+          <div className="flex flex-col gap-2">
+            <ContentAreaTitle>Students</ContentAreaTitle>
+            <ContentAreaDescription>
+              All student in this branch & semester
+            </ContentAreaDescription>
+          </div>
+          <Button className="font-semibold">
+            <PlusIcon className="size-4" />
+            Add Students
+          </Button>
         </ContentAreaHeader>
-        <ContentAreaContainer></ContentAreaContainer>
+        <ContentAreaContainer>
+          <div className="flex w-full gap-3">
+            <div className="relative flex w-full items-center">
+              <SearchIcon className="absolute ml-2.5 mr-2.5 size-4 text-muted-foreground" />
+              <Input placeholder="Search..." className="h-9 w-full ps-9" />
+            </div>
+          </div>
+        </ContentAreaContainer>
       </ContentArea>
     </div>
   );
