cbfc25beace9e88dba82b0b55448442de8c25ef4
x-sss-x <jbportalsblr@gmail.com>
2025-01-17 19:44:22 +0530

    feat: Update CreateOrganizationPage to display a message when no organization is created; comment out unused code

--- apps/nxss-app/src/app/(main)/(organization)/create-organization/[[...create-organization]]/page.tsx
+++ apps/nxss-app/src/app/(main)/(organization)/create-organization/[[...create-organization]]/page.tsx
@@ -1,23 +1,21 @@
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 
-import CreateOrganizationForm from "~/app/_components/forms/create-organization";
-
-const validSteps = ["", "exam-pattern", "welcome"];
+// const validSteps = ["", "exam-pattern", "welcome"];
 
 export default async function CreateOrganizationPage({
   params,
 }: {
   params: { step?: string[] };
 }) {
-  const currentStep = params?.step?.[0] || "";
+  // const currentStep = params?.step?.[0] || "";
 
-  if (!validSteps.includes(currentStep)) {
-    redirect("/create-organization");
-  }
+  // if (!validSteps.includes(currentStep)) {
+  //   redirect("/create-organization");
+  // }
 
   return (
     <div className="flex min-h-screen justify-center pt-28">
-      <CreateOrganizationForm />
+      No organizaiton created
     </div>
   );
 }
