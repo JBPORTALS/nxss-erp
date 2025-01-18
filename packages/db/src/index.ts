5b08905327435bf53e2d9be9707448f491a365cb
x-sss-x <jbportalsblr@gmail.com>
2025-01-18 17:09:52 +0530

    feat: update institution switcher styling and refactor db initialization

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -5,11 +5,9 @@ import ws from "ws";
 import { env } from "./env";
 import * as schema from "./schema";
 
-neonConfig.webSocketConstructor = ws; // <-- this is the key bit
-
 const pool = new Pool({ connectionString: env.DATABASE_URL });
 
-export const db = drizzle({ client: pool, schema, casing: "snake_case" });
+export const db = drizzle(pool, { schema, casing: "snake_case" });
 
 export * from "drizzle-orm";
 export * as schema from "./schema";
