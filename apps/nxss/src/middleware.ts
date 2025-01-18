909d684271afe09ae6ef74397a02b1b22a654726
x-sss-x <jbportalsblr@gmail.com>
2025-01-18 18:07:19 +0530

    feat: update global styles and enhance environment variable configuration

--- apps/nxss/src/middleware.ts
+++ apps/nxss/src/middleware.ts
@@ -11,12 +11,16 @@ const options = {
   isSatellite: true,
   signInUrl: env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
   domain: env.NEXT_PUBLIC_CLERK_DOMAIN,
+  secreteKey: env.CLERK_SECRET_KEY,
 };
 
-export default clerkMiddleware((auth, req) => {
-  if (isPublicRoute(req)) return NextResponse.next(); // if it's a public route, do nothing
-  auth().protect(); // for any other route, require auth
-}, options);
+export default clerkMiddleware(
+  (auth, req) => {
+    if (isPublicRoute(req)) return NextResponse.next(); // if it's a public route, do nothing
+    auth().protect(); // for any other route, require auth
+  },
+  { ...options },
+);
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
