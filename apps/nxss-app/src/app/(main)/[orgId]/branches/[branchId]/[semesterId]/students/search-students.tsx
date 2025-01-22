c6557e7502d08e73b12ced8f229cf6ad5efc45d2
x-sss-x <jbportalsblr@gmail.com>
2025-01-22 15:51:21 +0530

    feat: add clear button to search input and display status badge in student search

--- apps/nxss-app/src/app/(main)/[orgId]/branches/[branchId]/[semesterId]/students/search-students.tsx
+++ apps/nxss-app/src/app/(main)/[orgId]/branches/[branchId]/[semesterId]/students/search-students.tsx
@@ -7,9 +7,11 @@ import {
   PlusCircleIcon,
   SearchIcon,
   SquareIcon,
+  XIcon,
 } from "lucide-react";
 import { parseAsString, parseAsStringEnum, useQueryState } from "nuqs";
 
+import { Badge } from "@nxss/ui/badge";
 import { Button } from "@nxss/ui/button";
 import {
   Command,
@@ -20,6 +22,7 @@ import {
 } from "@nxss/ui/command";
 import { Input } from "@nxss/ui/input";
 import { Popover, PopoverContent, PopoverTrigger } from "@nxss/ui/popover";
+import { Separator } from "@nxss/ui/seperator";
 
 export default function SearchStudents() {
   const [query, setQuery] = useQueryState("q", parseAsString);
@@ -40,13 +43,31 @@ export default function SearchStudents() {
             })
           }
           placeholder="Search..."
-          className="h-9 w-full ps-9"
+          className="h-9 w-full pe-9 ps-9"
         />
+        {query && (
+          <Button
+            onClick={() => setQuery(null, { clearOnDefault: true })}
+            size={"icon"}
+            className="absolute right-0 ml-2.5 mr-2.5 size-5"
+            variant={"ghost"}
+          >
+            <XIcon className="size-3" />
+          </Button>
+        )}
       </div>
       <Popover>
         <PopoverTrigger asChild>
           <Button variant={"outline"} className="border-dashed">
-            <PlusCircleIcon className="size-4" /> Status
+            <PlusCircleIcon className="size-4" /> Status{" "}
+            {status ? (
+              <React.Fragment>
+                <Separator orientation="vertical" />
+                <Badge variant={"secondary"} className="capitalize">
+                  {status}
+                </Badge>
+              </React.Fragment>
+            ) : null}
           </Button>
         </PopoverTrigger>
         <PopoverContent align="start" className="w-[170px] p-0">
